f8723fae67a8c53c9bb8cb36b4d35d642013d850
Akk <antonio.jm.ferraz@gmail.com>
2024-03-23 00:23:18 +0000

    Add cards and image list

--- src/pages/Draft.tsx
+++ src/pages/Draft.tsx
@@ -1,6 +1,58 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Unstable_Grid2';
+import ChampSelectionList from '../components/ChampSelectionList';
+import SelectionChampCard from '../components/SelectionChampCard';
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
 export default function Draft() {
 
   return (
-    <div> Hey there this is Draft</div>
-  )
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={2}>
+        <Grid xs={3}>  
+          <Grid container spacing={2}>
+          <Grid>
+            <SelectionChampCard textSide='right'/>
+            </Grid><Grid>
+            <SelectionChampCard textSide='right'/>
+            </Grid><Grid>
+            <SelectionChampCard textSide='right'/>
+            </Grid><Grid>
+            <SelectionChampCard textSide='right'/>
+            </Grid><Grid>
+            <SelectionChampCard textSide='right'/>
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid xs={6} display="flex" justifyContent="center" alignItems="center">
+          <ChampSelectionList/>
+        </Grid>
+        <Grid xs={3}>  
+          <Grid container spacing={2}>
+          <Grid>
+            <SelectionChampCard textSide='left'/>
+            </Grid><Grid>
+            <SelectionChampCard textSide='left'/>
+            </Grid><Grid>
+            <SelectionChampCard textSide='left'/>
+            </Grid><Grid>
+            <SelectionChampCard textSide='left'/>
+            </Grid><Grid>
+            <SelectionChampCard textSide='left'/>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Box>
+  );
 }
